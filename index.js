eb21098b8ac1b9d75b7c120cd1354e3bb799857c
Dawood Alkawaz <dalkawaz@gmail.com>
2024-02-28 16:18:45 +0300

    Solution

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books').
+    then(res => res.json()).
+    then(res => renderBooks(res))
 }
 
 function renderBooks(books) {
